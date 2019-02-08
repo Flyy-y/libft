341248d426125547c77f030795aaad8c124ffef5
Clement BREISCH <cbreisch@e1r9p15.42.fr>
2019-02-08 02:22:08 +0100

    clear map ff

--- src/hashmap/ft_hashmap_clear.c
+++ src/hashmap/ft_hashmap_clear.c
@@ -6,7 +6,7 @@
 /*   By: cbreisch <cbreisch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 18:52:59 by cbreisch          #+#    #+#             */
-/*   Updated: 2019/02/08 00:43:53 by cbreisch         ###   ########.fr       */
+/*   Updated: 2019/02/08 02:21:33 by cbreisch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@ void	ft_hashmap_clear(t_hmap *map)
 {
 	while (map->items)
 		ft_hmap_delitem(map, map->items);
+	if (map->ff)
+		free(map->ff);
 	free(map);
 }
