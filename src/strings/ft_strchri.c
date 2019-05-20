a8fde35663309ff066c0c22594c4d34dd2dde5cf
Clement BREISCH <cbreisch@e1r5p1.42.fr>
2019-05-20 13:42:36 +0200

    .

--- src/strings/ft_strchri.c
+++ src/strings/ft_strchri.c
@@ -6,19 +6,14 @@
 /*   By: cbreisch <cbreisch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/16 13:22:57 by cbreisch          #+#    #+#             */
-/*   Updated: 2019/05/16 13:30:59 by cbreisch         ###   ########.fr       */
+/*   Updated: 2019/05/20 13:28:53 by cbreisch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_str.h"
+#include "ft_mem.h"
 
 int	ft_strchri(const char *s, int c)
 {
-	int	i;
-
-	i = -1;
-	while (s[++i])
-		if (s[i] == c)
-			return (i);
-	return (-1);
+	return (int)((char*)ft_memchr(s, c, ft_strlen(s) + 1) - s);
 }
